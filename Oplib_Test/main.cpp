680799289260686e491d10126e803ff26aef0f92
33 <yuluolfs@163.com>
2024-03-21 20:22:07 +0800

    添加libop的测试项目

---
+++ Oplib_Test/main.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+#include <time.h>
+#include <chrono>
+#include <filesystem>
+
+#include "op_test.h"
+
+#include <libop.h>
+
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+	locale loc("chs");
+	wcout.imbue(loc);
+
+	auto op = libop();
+	wcout << op.Ver() << endl;
+
+	test* ptest = new test;
+	ptest->do_test();
+	//delete ptest;
+	//ptest = nullptr;
+
+	system("pause");
+	return 0;
+}
